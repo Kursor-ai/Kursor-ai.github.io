d570385cd34c93241f2807db25902dca7ab0cadc
Nirmal Suthar <nirmalsuthar2008@gmail.com>
2023-05-03 01:52:16 +0530

    Add privacy policy page

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -40,12 +40,12 @@ function Header() {
 
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              {/* <li>
-                <Link to="/signin" className="font-medium text-purple-900 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+              <li>
+                <Link to="/" className="btn-sm text-white bg-purple-900 hover:bg-purple-700 ml-3">Home</Link>
               </li>
               <li>
-                <Link to="/signup" className="btn-sm text-white bg-purple-900 hover:bg-purple-700 ml-3">Sign up</Link>
-              </li> */}
+                <Link to="/privacy" className="font-medium text-purple-900 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">Privacy Policy</Link>
+              </li>
             </ul>
 
           </nav>
@@ -66,12 +66,12 @@ function Header() {
             {/*Mobile navigation */}
             <nav id="mobile-nav" ref={mobileNav} className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" style={mobileNavOpen ? { maxHeight: mobileNav.current.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: .8 } }>
               <ul className="bg-gray-800 px-4 py-2">
-                {/* <li>
-                  <Link to="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">Sign in</Link>
+                <li>
+                  <Link to="/" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Home</Link>
                 </li>
                 <li>
-                  <Link to="/signup" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Sign up</Link>
-                </li> */}
+                  <Link to="/privacy" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Privacy Policy</Link>
+                </li>
               </ul>
             </nav>
 
