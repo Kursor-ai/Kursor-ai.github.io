51370bae11d3e573ba9d3118f9def561485242a1
Nirmal Suthar <nirmalsuthar2008@gmail.com>
2023-05-04 03:01:43 +0530

    Fix routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider,
+  HashRouter as Router
+} from 'react-router-dom';
 import App from './App';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
