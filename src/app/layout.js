ad8c259e4624dee7b2f7115f088f9ec11e19c15d
Karan Singh <karanrsingh2662006@gmail.com>
2023-06-02 15:36:23 +0530

    added goofle analytics and sitemap

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,14 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: { default: "Kursor - Your AI Copilot", template: "%s | Kursor" },
   description: "Chat with ChatGPT or Google Bard AI while surfing the web !!!",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
- <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}/>
+      <GoogleAnalytics
+        GA_TRACKING_ID={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
+      />
       <body className={inter.className}>
         <Header />
         {children}
